634b6dd5f90a1cbcce6b5b8babe1fcb35bdf853b
Kelly Street <street.kelly@gmail.com>
2025-02-21 13:55:09 -0800

    Update pb_DE.R

--- pb_DE.R
+++ pb_DE.R
@@ -1,7 +1,9 @@
 cellcounts <- read.csv('data/2024KPMP_CellNumber_Subclassl1.csv', row.names = 1)
-names(cellcounts) <- c('cluster','sample','Freq')
+names(cellcounts) <- c('cluster','participant_id','Freq')
 
 counts <- read.csv('data/2024KPMP_Psuedobulk_Subclassl1.csv', row.names = 1)
+
+meta <- readRDS('data/simplified_KPMP_clinical_and_biomarker_data.rds')
 
 # outcome: gene expression
 # predictor: PFAS (continuous)
